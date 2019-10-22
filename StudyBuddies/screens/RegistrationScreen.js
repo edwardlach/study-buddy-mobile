7b02bbcc16111a4f8a461dec6e3f198580ab6fd0
Eesaa Philips <32400503+Ephilipz@users.noreply.github.com>
2019-10-22 22:37:52 +0200

    navigation between register and sign in

--- StudyBuddies/screens/RegistrationScreen.js
+++ StudyBuddies/screens/RegistrationScreen.js
@@ -3,59 +3,54 @@ import {
     View, Button, TextInput, Text
 } from 'react-native'
 import registrationStyles from '../styles/RegistrationStyles'
-import NavigationService from '../services/NavigationService'
 
-const RegistrationScreen = ({ onSubmit, onChangeText }) => {
-    return (
-        <View style={registrationStyles.container}>
+export default class RegistrationScreen extends React.Component {
+    render() {
+        return (
+            <View style={registrationStyles.container}>
 
-            <View style={registrationStyles.titleView}>
-                <Text style={registrationStyles.title}>REGISTER</Text>
-            </View>
-
-            <View style={registrationStyles.form}>
-                <TextInput style={registrationStyles.input}
-                    placeholder='Full Name'
-                    autoCapitalize="words"
-                    textContentType="name"
-                    onChangeText={val => onChangeText('name', val)} />
-
-                <TextInput style={registrationStyles.input}
-                    placeholder='Email'
-                    textContentType="emailAddress"
-                    onChangeText={val => onChangeText('email', val)} />
-
-                <TextInput style={registrationStyles.input}
-                    placeholder='Password'
-                    textContentType="password"
-                    secureTextEntry={true}
-                    onChangeText={val => onChangeText('password', val)} />
-            </View>
-
-            <View style={registrationStyles.actions}>
-                <View style={registrationStyles.buttonWrapper}>
-                    <Button
-                        title="Sign Up"
-                        onPress={() => onSubmit()}
-                    />
+                <View style={registrationStyles.titleView}>
+                    <Text style={registrationStyles.title}>REGISTER</Text>
                 </View>
 
-                <View style={registrationStyles.buttonWrapper}>
-                    <Button
-                        color="#e65058"
-                        title="Already Have An Account?"
-                    />
+                <View style={registrationStyles.form}>
+                    <TextInput style={registrationStyles.input}
+                        placeholder='Full Name'
+                        autoCapitalize="words"
+                        textContentType="name"
+                        onChangeText={val => this.props.onChangeText('name', val)} />
+
+                    <TextInput style={registrationStyles.input}
+                        placeholder='Email'
+                        textContentType="emailAddress"
+                        onChangeText={val => this.props.onChangeText('email', val)} />
+
+                    <TextInput style={registrationStyles.input}
+                        placeholder='Password'
+                        textContentType="password"
+                        secureTextEntry={true}
+                        onChangeText={val => this.props.onChangeText('password', val)} />
                 </View>
+
+                <View style={registrationStyles.actions}>
+                    <View style={registrationStyles.buttonWrapper}>
+                        <Button
+                            title="Sign Up"
+                            onPress={() => this.props.onSubmit()}
+                        />
+                    </View>
+
+                    <View style={registrationStyles.buttonWrapper}>
+                        <Button
+                            color="#e65058"
+                            title="Already Have An Account?"
+                            onPress={() => this.props.navigation.navigate('Signin')}
+                        />
+                    </View>
+                </View>
+
+
             </View>
-
-
-        </View>
-    )
+        )
+    }
 }
-
-RegistrationScreen.navigationOptions = {
-    title: 'register',
-};
-
-
-export default RegistrationScreen;
--- StudyBuddies/screens/SigninScreen.js
+++ StudyBuddies/screens/SigninScreen.js
@@ -3,52 +3,47 @@ import {
     View, Button, TextInput, Text
 } from 'react-native'
 import registrationStyles from '../styles/RegistrationStyles'
-import NavigationService from '../services/NavigationService'
 
+export default class SignInScreen extends React.Component {
+    render() {
+        return (
+            <View style={registrationStyles.container}>
 
-const SigninScreen = ({ onSubmit, onChangeText }) => {
-    return (
-        <View style={registrationStyles.container}>
-
-            <View style={registrationStyles.titleView}>
-                <Text style={registrationStyles.title}>Sign in</Text>
-            </View>
-
-            <View style={registrationStyles.form}>
-
-                <TextInput style={registrationStyles.input}
-                    placeholder='Email'
-                    textContentType="emailAddress"
-                    onChangeText={val => onChangeText('email', val)} />
-
-                <TextInput style={registrationStyles.input}
-                    placeholder='Password'
-                    textContentType="password"
-                    secureTextEntry={true}
-                    onChangeText={val => onChangeText('password', val)} />
-            </View>
-
-            <View style={registrationStyles.actions}>
-                <View style={registrationStyles.buttonWrapper}>
-                    <Button
-                        title="Sign In"
-                        onPress={() => onSubmit()}
-                    />
+                <View style={registrationStyles.titleView}>
+                    <Text style={registrationStyles.title}>Sign in</Text>
                 </View>
 
-                <View style={registrationStyles.buttonWrapper}>
-                    <Button
-                        color="#e65058"
-                        title="Don't have an account yet?"
-                    />
+                <View style={registrationStyles.form}>
+
+                    <TextInput style={registrationStyles.input}
+                        placeholder='Email'
+                        textContentType="emailAddress"
+                        onChangeText={val => this.props.onChangeText('email', val)} />
+
+                    <TextInput style={registrationStyles.input}
+                        placeholder='Password'
+                        textContentType="password"
+                        secureTextEntry={true}
+                        onChangeText={val => this.props.onChangeText('password', val)} />
+                </View>
+
+                <View style={registrationStyles.actions}>
+                    <View style={registrationStyles.buttonWrapper}>
+                        <Button
+                            title="Sign In"
+                            onPress={() => this.props.onSubmit()}
+                        />
+                    </View>
+
+                    <View style={registrationStyles.buttonWrapper}>
+                        <Button
+                            color="#e65058"
+                            title="Don't have an account yet?"
+                            onPress={() => this.props.navigation.navigate('Registration')}
+                        />
+                    </View>
                 </View>
             </View>
-        </View>
-    )
+        )
+    }
 }
-
-SigninScreen.navigationOptions = {
-    title: 'signin',
-};
-
-export default SigninScreen;
