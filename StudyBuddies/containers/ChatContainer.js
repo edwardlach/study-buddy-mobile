360e618b36e1c6a937a7c4461501a4cffbe68bdb
Edward Lach <edwarddlach@gmail.com>
2019-12-11 12:17:16 -0500

    Adding ChatContainer

---
+++ StudyBuddies/containers/ChatContainer.js
@@ -0,0 +1,55 @@
+import { connect } from 'react-redux';
+import CustomChatScreen from '../screens/CustomChatScreen';
+import { AsyncStorage } from 'react-native';
+import { sendMessage, updateMessage } from '../actions/messageActions';
+
+const getGroupId = state => {
+  var groupId;
+  state.groups.groupList.forEach(group => {
+    if (group.selected) {
+      groupId = group.id;
+    }
+  });
+  return groupId;
+}
+
+
+
+const mapStateToProps = state => {
+  var groupId = getGroupId(state);
+  return {
+    messages: state.messages.filter(
+      message => message.groupId === groupId
+    ),
+    userId: AsyncStorage.getItem('@UserId'),
+    groupId: groupId,
+    messageText: state.messageText
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sendMessage: (payload) => {
+      AsyncStorage.getItem('@UserId')
+        .then((userId) => {
+          dispatch(sendMessage(
+            {
+              ...payload,
+              userId: userId
+            }
+          ));
+        })
+        .then(() => {
+          dispatch(updateMessage(""));
+        });
+    },
+    updateMessageText: (text) => {
+      dispatch(updateMessage(text));
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CustomChatScreen)
